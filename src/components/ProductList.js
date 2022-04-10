c49350cb3796c67fe9bd2ab69c30c1126f637acd
Viviana Gomez <vivianakgp@gmail.com>
2022-04-09 19:27:30 -0500

    initial structure

---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+import '../styles/ProductList.css';
+
+
+const ProductList = () => {
+
+    return (
+        <div className="productList">
+            hello word
+        </div>
+    )
+
+}
+export default ProductList;
